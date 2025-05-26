126c36d3d2aa1ab5ee5f8a546febe001f3eff23d
Leo Fleischmann <54339115+leofleischmann@users.noreply.github.com>
2025-05-26 09:56:03 +0200

    Delete lib directory

--- lib/models/question_status.dart
+++
@@ -1,8 +0,0 @@
-// lib/models/question_status.dart
-
-enum QuestionStatus {
-  notAnswered, // Noch nicht angesehen oder keine Antwort gegeben
-  answered,    // Mindestens eine Option ausgewählt
-  skipped,     // Explizit übersprungen oder weitergegangen ohne Antwort (optional, kann auch notAnswered sein)
-  // Marked wird separat über den markedQuestionsProvider gehandhabt, kann aber hier für die UI kombiniert werden
-}
